ee732ae12b238f2c236c26147a70ca2ee3f14ac3
Robson-SM <profes_robson@hotmail.com>
2022-05-01 12:13:19 -0300

    Primeiro projeto Java

---
+++ primeiroProjeto/src/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args){
+        System.out.println("digite seu nome: ");
+
+        Scanner scan = new Scanner(System.in);
+        String nome = scan.nextLine();
+        System.out.println("Olá " +nome+ ", Seja bem Vindo!");
+
+    }
+
+}
---
+++ primeiroProjeto/src/SegundoPrograma.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+public class SegundoPrograma {
+
+    public static void main (String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        float nota1;
+        float nota2;
+        float nota3;
+        float nota4;
+
+        System.out.println("Digite a primeira nota: ");
+        nota1 = scan.nextFloat();
+
+        System.out.println("Digite a segunda nota: ");
+        nota2 = scan.nextFloat();
+
+        System.out.println("Digite a terceira nota: ");
+        nota3 = scan.nextFloat();
+
+        System.out.println("Digite a quarta nota: ");
+        nota4 = scan.nextFloat();
+
+        float media = (nota1+nota2+nota3+nota4)/4;
+
+        System.out.println("A media do aluno é: "+media);
+    }
+
+}
