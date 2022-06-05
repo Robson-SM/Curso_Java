d5a36d6d082a9329e31f629d0185ede9abed9660
Robson-SM <profes_robson@hotmail.com>
2022-06-05 10:00:13 -0300

    segundo exercicio de heranca

---
+++ heranca2/src/Veiculo.java
@@ -0,0 +1,13 @@
+public class Veiculo {
+    protected String tipoDeCombustivel;
+    protected int porcentagemDeCombustivelNoTanque;
+
+    public Veiculo(){
+    }
+
+    public Veiculo(String tipoDeCombustivel, int porcentagemDeCombustivelNoTanque){
+        this.tipoDeCombustivel = tipoDeCombustivel;
+        this.porcentagemDeCombustivelNoTanque = porcentagemDeCombustivelNoTanque;
+
+    }
+}
